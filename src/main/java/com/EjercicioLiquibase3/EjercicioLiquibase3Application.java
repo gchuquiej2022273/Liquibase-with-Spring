92f02b16dc3273d76a7f14ae6c149ca6badec410
gchuquiej-2022273 <gchuquiej-2022273@kinal.edu.gt>
2024-09-16 10:05:37 -0600

    Se finalizo el ejercicio

---
+++ src/main/java/com/EjercicioLiquibase3/EjercicioLiquibase3Application.java
@@ -0,0 +1,13 @@
+package com.EjercicioLiquibase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioLiquibase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioLiquibase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EjercicioLiquibase3/EjercicioLiquibase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjercicioLiquibase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioLiquibase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
